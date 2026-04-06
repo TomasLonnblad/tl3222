81723e2ba9a98bb5aac29898571933b3489b46d8
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-06 14:49:35 +0200

    xxx

--- components/globe-demo.tsx
+++ components/globe-demo.tsx
@@ -396,7 +396,7 @@ export default function GlobeDemo() {
 
   return (
     <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
-      <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
+      <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full  px-4">
         <motion.div
           initial={{
             opacity: 0,
