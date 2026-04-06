2b6dc3d7ab2fe68a4c2b57566f91ef28e8177a99
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-06 14:27:22 +0200

    x

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Component from '@/components/globe-demo'
+
+export default function Page() {
+  return (
+    <main className="flex min-h-svh items-center justify-center">
+      <Component />
+    </main>
+  )
+}
